1840cfae3f73e4aa284e730f64292af406a85763
Tiago Cruz <tiagocruz@MBP-de-Tiago.lan>
2017-10-07 11:07:00 +0100

    Fix web view unit test

--- app/src/androidTest/java/co/base/androidbaseapplication/ui/countrydetails/CountryDetailActivityTest.java
+++ app/src/androidTest/java/co/base/androidbaseapplication/ui/countrydetails/CountryDetailActivityTest.java
@@ -48,7 +48,7 @@ public class CountryDetailActivityTest
         Espresso.onView( withId( R.id.webView ) )
                 .check( matches( isDisplayed( ) ) );
 
-        onWebView( withId( R.id.webView ) ).withTimeout( 1, TimeUnit.MINUTES )
+        onWebView( withId( R.id.webView ) ).withTimeout( 2, TimeUnit.MINUTES )
                 .check( webMatches( getCurrentUrl( ), containsString( "country.io/portugal" ) ) );
 
     }
