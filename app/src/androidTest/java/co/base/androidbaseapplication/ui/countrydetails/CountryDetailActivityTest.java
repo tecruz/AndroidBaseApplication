fdbdc0032d1ce8886dcae3c74856d0aa95875e8d
Tiago Cruz <tiagocruz@MBP-de-Tiago.lan>
2017-10-09 21:36:10 +0100

    Fix  build

--- app/src/androidTest/java/co/base/androidbaseapplication/ui/countrydetails/CountryDetailActivityTest.java
+++ app/src/androidTest/java/co/base/androidbaseapplication/ui/countrydetails/CountryDetailActivityTest.java
@@ -11,14 +11,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.concurrent.TimeUnit;
-
 import co.base.androidbaseapplication.R;
 
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class CountryDetailActivityTest
